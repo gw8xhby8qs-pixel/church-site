80e63e698fd62a2f08248bc8f2623a7189681182
Вячеслав Клєщов <vaceslavklesov@MacBook-Air-Vaceslav.fritz.box>
2026-03-21 20:18:01 +0100

    Fix app file, update routes for Railway

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 # Главная
-@app.route("/index")
-def index():
+@app.route("/")
+def home():
     return render_template("index.html")
 
 
@@ -50,5 +50,8 @@ def about():
     return render_template("about.html")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
